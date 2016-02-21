7974b2409efadd658365328b66eb3b5c0d10f910
alhimik1986 <sidorovich21101986@mail.ru>
2016-02-21 18:13:43 +0800

    added instruction to readme.md

--- Запустить сервер.bat
+++ Запустить сервер.bat
@@ -1,6 +1,13 @@
+@rem Останавливаю службу "NginxTrayRu", если она есть
 @net stop NginxTrayRu
+
+@rem Перехожу в нужную папку патча и запускаю нужный патч
 @cd %~dp0\web-server\patches\patch
 call patch.bat
+
+@rem Перехожу в нужную папку трея и запускаю трей с нужными нам компонентами веб-сервера
 @cd ..\..\trays
 start NginxTrayRu.exe
+
+@rem Открываю браузер с адресом http://localhost
 start http://localhost
